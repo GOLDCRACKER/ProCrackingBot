e9d24a69742f5cf313ef80fedf51c1131143c30a
GOLDCRACKER <GOLDCRACKER9263@GmaiL.CoM>
2018-09-07 18:04:20 +0200

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,5 +1,5 @@
 -- #Beyond Reborn Robot
--- #@BeyondTeam
+-- #@ProCracking
 
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
@@ -94,24 +94,22 @@ function create_config( )
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v6.0
+    info_text = [[》GOLDCRACKER V1.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》https://github.com/GOLDCRACKER/ProCracking
 
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
+》@typing1 ➣ Founder & Developer《
+》@GOLDCRACKER ➣ Developer《
+》@Sir_ProgrammER ➣ Developer《
 
 》Special thanks to :
-》Beyond Team Members
+》ProCracking Team Members
 
 》Our channel :
-》@BeyondTeam《
+》@ProCracking《
 
-》Our website :
-》http://Beyond-Dev.iR
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
@@ -153,7 +151,7 @@ function load_plugins()
 			print('\27[31m'..err..'\27[39m')
 		end
 	end
-	print('\n'..#config.enabled_plugins..' Plugins Are Active\n\nStarting BDReborn Robot...\n')
+	print('\n'..#config.enabled_plugins..' Plugins Are Active\n\nStarting ProCracking Robot...\n')
 end
 
 load_plugins()
